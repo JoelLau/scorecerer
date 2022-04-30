cb97167bc26c0cc8cc70ef916e73a11a688e8397
Joel Lau <29514264+JoelLau@users.noreply.github.com>
2022-04-30 18:36:16 +0800

    basic form elements

--- apps/scorecerer/src/app/app.tsx
+++ apps/scorecerer/src/app/app.tsx
@@ -1,17 +1,74 @@
-import { Message } from '@scorecerer/api-interfaces';
-import React, { useEffect, useState } from 'react';
-import NxWelcome from './nx-welcome';
+import React from 'react';
 
 export const App = () => {
-  const [{ title }, setMessage] = useState<Message>({ title: '' });
-
-  useEffect(() => {
-    fetch('/api')
-      .then((r) => r.json())
-      .then(setMessage);
-  }, []);
-
-  return <NxWelcome title={title}></NxWelcome>;
+  return (
+    <>
+      <form id="form">
+        <div className="popularity">
+          <label htmlFor="popularity">Popularity</label>
+          <input
+            type="number"
+            name="popularity"
+            id="popularity"
+            placeholder="Popularity"
+            min="0"
+            max="18"
+            value="0"
+          />
+        </div>
+        <div className="stars">
+          <label htmlFor="stars">Star Tokens Placed</label>
+          <input
+            type="number"
+            name="stars"
+            id="stars"
+            placeholder="Star Tokens Placed"
+            min="0"
+            max="6"
+            value="0"
+          />
+        </div>
+        <div className="territories">
+          <label htmlFor="territories">Territories Controlled</label>
+          <input
+            type="number"
+            name="territories"
+            id="star-tokens"
+            placeholder="Territories Controlled"
+            min="0"
+            value="0"
+          />
+        </div>
+        <div className="resources">
+          <label htmlFor="resources">Resources Controlled</label>
+          <input
+            type="number"
+            name="resources"
+            id="resources"
+            placeholder="Resources Controlled"
+            min="0"
+            max="6"
+            value="0"
+          />
+        </div>
+        <div className="structure-bonus">
+          <label htmlFor="structure-bonus">Structure Bonus</label>
+          <input
+            type="number"
+            name="structure-bonus"
+            id="structure-bonus"
+            placeholder="Structure Bonus"
+            min="0"
+            value="0"
+          />
+        </div>
+        <div className="complete">
+          <button type="submit">Calculate</button>
+        </div>
+      </form>
+      <div className="results">some final score here</div>
+    </>
+  );
 };
 
 export default App;
