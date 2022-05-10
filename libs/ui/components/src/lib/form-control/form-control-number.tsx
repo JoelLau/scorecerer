78b47dd941eda3ebf1cae1d45cd8cb2066f5a184
Joel Lau <29514264+JoelLau@users.noreply.github.com>
2022-05-10 23:14:34 +0800

    styles number input

--- libs/ui/components/src/lib/form-control/form-control-number.tsx
+++ libs/ui/components/src/lib/form-control/form-control-number.tsx
@@ -7,10 +7,24 @@ export interface FormControlNumberProps extends FormControlBaseProps {
   name?: string;
   placeholder?: string;
   autoFocus?: boolean;
+  className?: string;
 }
 
-export function FormControlNumber(props: FormControlNumberProps) {
-  return <input type="number" data-testid="form-control-number" {...props} />;
+export function FormControlNumber({
+  className,
+  ...props
+}: FormControlNumberProps) {
+  return (
+    <input
+      type="number"
+      className={
+        'mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm border-gray-300 rounded-md p-2 pr-0' +
+          className || ''
+      }
+      data-testid="form-control-number"
+      {...props}
+    />
+  );
 }
 
 export default FormControlNumber;
